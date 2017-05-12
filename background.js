916ffd959e689443ed6698bdbf6918151bfb5a54
dkorosec <dominik.korosec@gmail.com>
2017-05-12 19:40:18 +0200

    mini fixes

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ const second = 1000;
 const variate = second * 10;
 
 function httpGetAsync(url, callback) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange =  () => {
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = () => {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
       callback(xmlHttp.responseText);
   }
@@ -11,22 +11,22 @@ function httpGetAsync(url, callback) {
   xmlHttp.send(null);
 }
 
-function trySendRequestLoop(){
-  
-  var time = second * 50 + Math.floor(Math.random()*variate);
+function trySendRequestLoop() {
+  let time = second * 50 + Math.floor(Math.random() * variate);
   chrome.tabs.query({
   }, (tabs) => {
-    for (var tab of tabs) {
-      var url = tab.url;
+    for (let tab of tabs) {
+      let url = tab.url;
       if (url.includes('vaje.um.si') || url.includes('vaje.uni-mb.si')) {
-        httpGetAsync("https://vaje.um.si/vaje/new/vaje.jsp",(data)=>console.log("ping send!"));
+        httpGetAsync('https://vaje.um.si/vaje/new/vaje.jsp', (data) => console.log('ping send!'));
         return;
       }
     }
-    console.log("vaje uni not found in tabs!");
+    console.log('vaje uni not found in tabs!');
   });
-  console.log(`resending in ${time/1000} seconds!`);
-  setTimeout(trySendRequestLoop,time);
+
+  console.log(`resending in ${time / 1000} seconds!`);
+  setTimeout(trySendRequestLoop, time);
 }
 
 trySendRequestLoop();
